aafa89a74d9e719da0ef036abf61f8324dd567c8
Nazmul Alom <nazmulshuvo03@gmail.com>
2019-04-12 02:13:56 +0600

    paypal payment button added and connected with paypal developer account

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,7 +19,7 @@ export default class Cart extends Component {
 									<Title name="mojar" title="cart" />
 									<CartColumns />
 									<CartList value={value} />
-									<CartTotals value={value} />
+									<CartTotals value={value} history={this.props.history} />
 								</React.Fragment>
 							);
 						} else {
