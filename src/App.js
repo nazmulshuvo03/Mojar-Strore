38022192f482519213c5580471abb61d05893a46
Nazmul Alom <nazmulshuvo03@gmail.com>
2020-01-09 18:50:45 +0600

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from './components/Navbar';
-import ProductList from './components/ProductList';
-import Details from './components/Details';
-import Cart from './components/Cart';
-import Default from './components/Default';
-import Modal from './components/Modal';
-import FloatingButton from './components/FloatingButton';
-import About from './components/About';
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Modal from "./components/Modal";
+import About from "./components/About";
 
 class App extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Navbar />
-				<Switch>
-					<Route exact path="/" component={ProductList} />
-					<Route path="/details" component={Details} />
-					<Route path="/cart" component={Cart} />
-					<Route path="/about" component={About} />
-					<Route component={Default} />
-				</Switch>
-				<Modal />
-				<FloatingButton />
-			</React.Fragment>
-		);
-	}
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={ProductList} />
+                    <Route path="/details" component={Details} />
+                    <Route path="/cart" component={Cart} />
+                    <Route path="/about" component={About} />
+                    <Route component={Default} />
+                </Switch>
+                <Modal />
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
